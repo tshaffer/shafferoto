dffb9168be6d61c3321e7e4486718f8010f1045e
Ted Shaffer <ted@brightsign.biz>
2016-03-13 12:14:38 -0700

    disable timer; avoid exception by not setting attribute on undefined item

--- angularClient/app.js
+++ angularClient/app.js
@@ -14,14 +14,7 @@ shafferoto.controller('ShafferotoController', ['$scope', '$http', 'shafferotoSer
             var url = baseUrl + photo.url;
             $scope.images.push( { src: url, title: photo.title, orientation: photo.orientation } );
         });
-
     })
-
-    //var baseDir = 'http://localhost:3000/assets/';
-    //var picture1 = baseDir + "IMG_1624.JPG";
-    //var picture2 = baseDir + 'IMG_1625.JPG';
-    //$scope.images = [ { src: picture1, title: 'Picture 1'}, { src: picture2, title: 'Picture 2'}];
-    //$scope.images=[{src:'img1.png',title:'Pic 1'},{src:'img2.jpg',title:'Pic 2'},{src:'img3.jpg',title:'Pic 3'},{src:'img4.png',title:'Pic 4'},{src:'img5.png',title:'Pic 5'}];
 }]);
 
 shafferoto.directive('slider', function ($timeout) {
@@ -47,25 +40,30 @@ shafferoto.directive('slider', function ($timeout) {
                 scope.images.forEach(function(image){
                     image.visible=false;
                 });
-                scope.images[scope.currentIndex].visible=true;
+                if (typeof scope.images[scope.currentIndex] != 'undefined') {
+                    scope.images[scope.currentIndex].visible = true;
+                }
+                else {
+                    console.log("images undefined");
+                }
             });
 
             /* Start: For Automatic slideshow*/
 
             var timer;
 
-            var sliderFunc=function(){
-                timer=$timeout(function(){
-                    scope.next();
-                    timer=$timeout(sliderFunc,15000);
-                },15000);
-            };
-
-            sliderFunc();
-
-            scope.$on('$destroy',function(){
-                $timeout.cancel(timer);
-            });
+            //var sliderFunc=function(){
+            //    timer=$timeout(function(){
+            //        scope.next();
+            //        timer=$timeout(sliderFunc,15000);
+            //    },15000);
+            //};
+            //
+            //sliderFunc();
+            //
+            //scope.$on('$destroy',function(){
+            //    $timeout.cancel(timer);
+            //});
 
             /* End : For Automatic slideshow*/
 
