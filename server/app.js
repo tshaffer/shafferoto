dc21a21d7aee78c969904948b0ca0414fcd5cb95
Ted Shaffer <ted@brightsign.biz>
2016-03-13 08:02:20 -0700

    Remove boilerplate code added by WebStorm. Start with semi-clean state.

--- server/app.js
+++ server/app.js
@@ -1,68 +1,21 @@
 var express = require('express');
-
+//var request = require('request');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var fs = require("fs");
 
 //https://github.com/gomfunkel/node-exif
 var ExifImage = require('exif').ExifImage;
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use('/assets', express.static(path.join(__dirname,'/public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', function(req, res) {
+  res.send('<html><head></head><body><h1>Hello shafferoto!</h1></body></html>');
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-module.exports = app;
 
 // after boilerplate code
 var mongoose = require('mongoose');
@@ -70,9 +23,6 @@ var mongoose = require('mongoose');
 console.log("launch shafferoto");
 
 mongoose.connect('mongodb://localhost/shafferotoTest');
-
-var app = express();
-module.exports = app;
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -190,6 +140,8 @@ function getExifData(photos, photoIndex) {
 
 function savePhotosToDB(photos) {
 
+  return;
+
   photos.forEach(function(photo) {
 
     var photoForDB = new Photo({
@@ -229,3 +181,8 @@ function handleError(err) {
   console.log("handleError invoked");
   return;
 }
+
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+
