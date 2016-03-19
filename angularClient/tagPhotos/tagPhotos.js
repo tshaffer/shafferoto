112c42514943143a04e64cd01c4b5a8dd159874d
Ted Shaffer <ted@brightsign.biz>
2016-03-19 08:20:43 -0700

    great progress!

--- angularClient/tagPhotos/tagPhotos.js
+++ angularClient/tagPhotos/tagPhotos.js
@@ -1,42 +1,43 @@
 angular.module('shafferoto').controller('tagPhotos', ['$scope', 'shafferotoServerService', function($scope, $shafferotoServerService ) {
 
-    $scope.myChildren = [
-        {
-            "firstName": "Sam",
-            "lastName": "Shaffer",
-            "image": "img/img1.png",
-            "image2": "img/img4.png",
-        },
-        {
-            "firstName": "Joel",
-            "lastName": "Shaffer",
-            "image": "img/img2.jpg",
-            "image2": "img/img5.png",
-        },
-        {
-            "firstName": "Rachel",
-            "lastName": "Shaffer",
-            "image": "img/img3.jpg",
-            "image2": "img/img1.png",
-        }];
+    $scope.photos = [];
 
     $scope.gridOptions = {
         modifierKeysToMultiSelectCells: true,
-        //enableSorting: true,
-        //enableCellSelection: true,
-        //enableRowSelection: false,
-        ////enableCellEditOnFocus: true,
-        //enableCellEdit: true,
+        rowHeight:200,
         columnDefs: [
-            { name: 'First', field: 'firstName' },
-            //{ name: 'Photo', field: 'image', cellTemplate:"<div><p>foo</p> <img width=\"50px\" ng-src=\"{{grid.getCellValue(row, col)}}\" lazy-src></div>"},
-            //{ name: 'Photo', field: 'image', cellTemplate:"<img width=\"50px\" ng-src=\"{{grid.getCellValue(row, col)}}\" lazy-src>"},
-            { name: 'Photo', field: 'image', cellTemplate:"<div class='ui-grid-cell-contents'><img width=\"50px\" ng-src=\"{{grid.getCellValue(row, col)}}\" lazy-src> </div>"},
-            { name: 'Last', field: 'lastName'  },
-            { name: 'Photo2', field: 'image2', cellTemplate:"<div class='ui-grid-cell-contents'><img width=\"50px\" ng-src=\"{{grid.getCellValue(row, col)}}\" lazy-src> </div>"}
+            { name: 'Photo', field: 'image', cellTemplate:"<div class='ui-grid-cell-contents'><img width=\"200px\" ng-src=\"{{grid.getCellValue(row, col)}}\" lazy-src> </div>"},
+            { name: 'Photo2', field: 'image2', cellTemplate:"<div class='ui-grid-cell-contents'><img width=\"200px\" ng-src=\"{{grid.getCellValue(row, col)}}\" lazy-src> </div>"}
         ],
     };
 
-    $scope.gridOptions.data = $scope.myChildren;
+    $scope.gridOptions.data = $scope.photos;
+
+    var getPhotosPromise = $shafferotoServerService.getPhotos();
+    getPhotosPromise.then(function (result) {
+        console.log("getPhotos successful");
+
+        var baseUrl = $shafferotoServerService.getBaseUrl() +  "photos/";
+
+        var firstPhoto = true;
+        var image1;
+        var image2;
+
+        result.data.photos.forEach(function(photo){
+
+            var url = baseUrl + photo.url;
+
+            if (firstPhoto) {
+                image1 = url;
+            }
+            else {
+                photo = { "image": image1, "image2": url };
+                $scope.photos.push(photo);
+            }
+            firstPhoto = !firstPhoto;
+        });
+
+        //$scope.$broadcast("imagesInitialized");
+    })
 
 }]);
