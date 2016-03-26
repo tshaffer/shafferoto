98baf08fad6508f04ac5da71bbc744fea5b56d62
Ted Shaffer <ted@brightsign.biz>
2016-03-26 07:18:39 -0700

    builds thumbs (in progress)

--- server/app.js
+++ server/app.js
@@ -81,13 +81,12 @@ app.get('/updateTags', function (req, res) {
   res.send("tags updated");
 });
 
-
 app.get('/updateDB', function(req, res) {
 
   console.log("updateDB invoked");
   res.set('Access-Control-Allow-Origin', '*');
 
-  // retrieve photos that exist in db; get them in a hash table
+// retrieve photos that exist in db; get them in a hash table
   var hashAllPhotosPromise = dbController.hashAllPhotos();
   hashAllPhotosPromise.then(function(photosInDB) {
 
@@ -100,56 +99,178 @@ app.get('/updateDB', function(req, res) {
       // look for photosOnDrive that aren't in photosInDB
       var photosToAdd = [];
       photosOnDrive.forEach(function (photoOnDrive) {
-        if (!photosInDB.hasOwnProperty(photoOnDrive.url)) {
+        if ( !photosInDB.hasOwnProperty( photoOnDrive.url ) ) {
           photosToAdd.push(photoOnDrive);
         }
       });
 
-      if (photosToAdd.length <= 0) {
-      }
-      else {
-        var photo = photosToAdd[0];
-        var filePath = photo.filePath;
-        var getInfoPromise = easyImage.info(filePath);
-        getInfoPromise.then(function (photoInfo) {
-          console.log("returned from easyImage");
+      if (photosToAdd.length > 0) {
+        var getExifDataPromise = exifReader.getAllExifData(photosToAdd);
+        getExifDataPromise.then(function(photos) {
+          console.log("getExifDataPromised resolved");
 
-          var height = photoInfo.height;
-          var width = photoInfo.width;
-
-          var ratio = height / width;
-
-          var heightRatio = height / 250;
-
-          var targetWidth = width / (height / 250);
-          var targetHeight = 250;
-
-          var createThumbPromise = easyImage.resize({
-            src: filePath,
-            dst: "foo.jpg",
-            width: targetWidth,
-            height: targetHeight
-          });
-          createThumbPromise.then(function (image) {
-            console.log("created thumbnail");
+          var buildThumbnailsPromise = buildThumbnails(photos);
+          buildThumbnailsPromise.then(function(obj) {
+            console.log("thumbnails build complete");
           });
 
-          //easyimg.resize({src:srcimg, dst: __dirname + '/output/resize.jpg', width:640, height:400}).then(function (file) {});
-
-          //var getExifDataPromise = exifReader.getAllExifData(photosToAdd);
-          //getExifDataPromise.then(function(photos) {
-          //  console.log("getExifDataPromised resolved");
+          //photos.forEach(function(photo) {
+          //  console.log("create thumb for photo");
           //
+          //  //dateTaken:2013-10-19T21:02:52.183Z
+          //  //filePath:"/Users/tedshaffer/Documents/Projects/shafferoto/server/public/testPhotos/Tahoe/p... (length: 93)"
+          //  //imageHeight:2448
+          //  //imageWidth:3264
+          //  //orientation:3
+          //  //title:"photo (10).JPG"
+          //  //url:"testPhotos/Tahoe/photo (10).JPG"
           //
-          //  dbController.savePhotosToDB(photos);
+          //  var targetHeight = 250;
+          //  var heightRatio = photo.imageHeight / targetHeight;
+          //  var targetWidth = photo.imageWidth / (photo.imageHeight / targetHeight);
+          //
+          //  var createThumbPromise = easyImage.resize({
+          //    src: photo.filePath,
+          //    dst: "foo.jpg",
+          //    width: targetWidth,
+          //    height: targetHeight
+          //  });
+          //  createThumbPromise.then(function (image) {
+          //    console.log("created thumbnail");
+          //  });
           //});
+          //
+          ////dbController.savePhotosToDB(photos);
         });
       }
-
-      res.send("ok");
     }
+
+    res.send("ok");
   });
 });
+
+
+function buildThumbnails(photos) {
+
+  var photoCount = photos.length;
+
+  return new Promise(function(resolve, reject) {
+
+    var sequence = Promise.resolve();
+
+    photos.forEach(function(photo) {
+      // Add these actions to the end of the sequence
+      sequence = sequence.then(function() {
+        return buildThumb(photo);
+      }).then(function(photo) {
+        //photosWithExifData.push(photo);
+        photoCount--;
+        console.log("photoCount=" + photoCount);
+        if (photoCount == 0) {
+          resolve(null);
+        }
+      });
+    });
+  });
+}
+
+
+function buildThumb(photo) {
+
+  return new Promise(function(resolve, reject) {
+
+    var targetHeight = 250;
+    //var heightRatio = photo.imageHeight / targetHeight;
+    var targetWidth = photo.imageWidth / (photo.imageHeight / targetHeight);
+
+    var dirName = path.dirname(photo.filePath);
+    var fileName = path.basename(photo.filePath);
+    var ext = path.extname(photo.filePath);
+
+    var thumbFileName = fileName.substring(0,fileName.length - ext.length)+"_thumb" + ext;
+    var thumbPathName = path.join(dirName,thumbFileName);
+
+    var createThumbPromise = easyImage.resize({
+      src: photo.filePath,
+      dst: thumbPathName,
+      width: targetWidth,
+      height: targetHeight
+    });
+    createThumbPromise.then(function (image) {
+      console.log("created thumbnail");
+      resolve(image);
+    });
+  });
+}
+
+//app.get('/updateDB', function(req, res) {
+//
+//  console.log("updateDB invoked");
+//  res.set('Access-Control-Allow-Origin', '*');
+//
+//  // retrieve photos that exist in db; get them in a hash table
+//  var hashAllPhotosPromise = dbController.hashAllPhotos();
+//  hashAllPhotosPromise.then(function(photosInDB) {
+//
+//    console.log('Look for photos in ' + photosDir);
+//    var photosOnDrive = [];
+//    photosOnDrive = findPhotos(photosDir, photosOnDrive);
+//
+//    if (photosOnDrive.length > 0) {
+//
+//      // look for photosOnDrive that aren't in photosInDB
+//      var photosToAdd = [];
+//      photosOnDrive.forEach(function (photoOnDrive) {
+//        if (!photosInDB.hasOwnProperty(photoOnDrive.url)) {
+//          photosToAdd.push(photoOnDrive);
+//        }
+//      });
+//
+//      if (photosToAdd.length <= 0) {
+//      }
+//      else {
+//        var photo = photosToAdd[0];
+//        var filePath = photo.filePath;
+//        var getInfoPromise = easyImage.info(filePath);
+//        getInfoPromise.then(function (photoInfo) {
+//          console.log("returned from easyImage");
+//
+//          var height = photoInfo.height;
+//          var width = photoInfo.width;
+//
+//          var ratio = height / width;
+//
+//          var heightRatio = height / 250;
+//
+//          var targetWidth = width / (height / 250);
+//          var targetHeight = 250;
+//
+//          var createThumbPromise = easyImage.resize({
+//            src: filePath,
+//            dst: "foo.jpg",
+//            width: targetWidth,
+//            height: targetHeight
+//          });
+//          createThumbPromise.then(function (image) {
+//            console.log("created thumbnail");
+//          });
+//
+//          //easyimg.resize({src:srcimg, dst: __dirname + '/output/resize.jpg', width:640, height:400}).then(function (file) {});
+//
+//          //var getExifDataPromise = exifReader.getAllExifData(photosToAdd);
+//          //getExifDataPromise.then(function(photos) {
+//          //  console.log("getExifDataPromised resolved");
+//          //
+//          //
+//          //  dbController.savePhotosToDB(photos);
+//          //});
+//        });
+//      }
+//
+//      res.send("ok");
+//    }
+//  });
+//});
 
 app.get('/getTaggedPhotos', function(req, res) {
   console.log("specified tag is " + req.query.tag);
