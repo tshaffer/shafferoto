85d4062e6072ddc9d7c266c4bb9710dada36391a
Ted Shaffer <ted@brightsign.biz>
2016-05-27 20:52:11 -0700

    code that works but I don't understand why :(

--- server/app.js
+++ server/app.js
@@ -39,10 +39,38 @@ openDBPromise.then(function() {
   var fetchPhotoFoldersPromise = dbController.fetchPhotoFolders();
   fetchPhotoFoldersPromise.then(function(photoFolders) {
     photoFolders.forEach(function(photoFolder, index, array) {
-      app.use(express.static(photoFolder.dirname));
+      // app.use(express.static(photoFolder.dirname));
+      app.use(express.static(photoFolder.dirName));
+      // app.use(express.static(photoFolder.baseName));
     });
   });
 });
+
+// openDBPromise.then(function() {
+//   var fetchPhotoFoldersPromise = dbController.fetchPhotoFolders();
+//   fetchPhotoFoldersPromise.then(function(photoFolders) {
+//     photoFolders.forEach(function(photoFolder, index, array) {
+//       // app.use("file:///" + photoFolder.baseName, express.static(photoFolder.baseName));
+//       // app.use("file:///" + photoFolder.baseName, express.static(photoFolder.baseName, {
+//       //   setHeaders: function(res, filePath) {
+//       //     debugger;
+//       //   }
+//       // }));
+//       // app.use("file:///" + photoFolder.baseName, function(req, res) {
+//       //   debugger;
+//       // })
+//       // app.use("/SanMateoCoast2013/IMG_7093_thumb.JPG", function(req, res) {
+//       //   debugger;
+//       // });
+//       //
+//       // app.use("/", function(req, res) {
+//       //   console.log("eat pizza");
+//       // });
+//
+//
+//     });
+//   });
+// });
 
 
 app.get('/getPhotos', function(req, res) {
@@ -438,7 +466,44 @@ function handleError(err) {
   return;
 }
 
+// app.use("/SanMateoCoast2013/IMG_7093_thumb.JPG", function(req, res) {
+//   debugger;
+// });
+//
+// app.use("file:///SanMateoCoast2013/IMG_7093_thumb.JPG", function(req, res) {
+//   debugger;
+// });
+//
+// app.use("/", function(req, res) {
+//   debugger;
+// });
+//
+// app.use("file://", function(req, res) {
+//   debugger;
+// });
+//
+// app.use("file:///", function(req, res) {
+//   debugger;
+// });
+
+// var http = require('http');
+//
+// http.createServer(function(request, response) {
+//   var headers = request.headers;
+//   var method = request.method;
+//   var url = request.url;
+//   console.log(url);
+//   var body = [];
+//   request.on('error', function(err) {
+//     console.error(err);
+//   }).on('data', function(chunk) {
+//     body.push(chunk);
+//   }).on('end', function() {
+//     body = Buffer.concat(body).toString();
+//     // At this point, we have the headers, method, url and body, and can now
+//     // do whatever we need to in order to respond to this request.
+//   });
+// }).listen(3000); // Activates this server, listening on port 8080.
 
 var port = process.env.PORT || 3000;
 app.listen(port);
-
